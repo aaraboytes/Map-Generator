55d0eb7e1a70133037818baa78969c445ea32ed0
chetobros <chetobros@gmail.com>
2018-06-07 00:50:12 -0500

    MapGen
    WIP

---
+++ Assets/Scripts/RoomTemplates.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour {
+	public GameObject[] rightRooms;
+	public GameObject[] leftRooms;
+	public GameObject[] topRooms;
+	public GameObject[] bottomRooms;
+}
